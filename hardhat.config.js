904057cd516fc651ec56e97572a453d7b860a06c
Kevin Beauregard <kbeau@regard.com>
2021-07-15 12:17:31 -0400

    add deployment infrastructure

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,52 @@
 'use strict'
-
-const {config} = require('dotenv')
-config()
+require('dotenv').config()
 
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-etherscan')
-require('@openzeppelin/hardhat-upgrades')
 require('hardhat-contract-sizer')
 require('solidity-coverage')
+require('hardhat-deploy')
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY || ''
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || ''
-const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || ''
-const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || ''
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
-const LOCAL_PRIVATE_KEY = process.env.LOCAL_PRIVATE_KEY || ''
+const gasPrice = 55000000000
 
 module.exports = {
   defaultNetwork: 'hardhat',
-  solidity: '0.8.3',
   networks: {
     hardhat: {},
-    coverage: {
-      url: 'http://127.0.0.1:8555', // Coverage launches its own ganache-cli client
+    localhost: {
+      saveDeployments: true
     },
+    coverage: {
+      url: 'http://127.0.0.1:8555' // Coverage launches its own ganache-cli client
+    },
+    mainnet: {
+      url: process.env.NODE_URL,
+      chainId: 1,
+      gasPrice
+    }
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY,
+  paths: {
+    deployments: 'deployments'
+  },
+  namedAccounts: {
+    deployer: process.env.DEPLOYER || 0
+  },
+  solidity: {
+    version: '0.8.3',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   },
   contractSizer: {
     alphaSort: true,
     runOnCompile: true,
-    disambiguatePaths: false,
+    disambiguatePaths: false
+  },
+  mocha: {
+    timeout: 200000
   }
 }
