f665f2cf1b9d27b490a7238f08d23f554aa0d74c
Rohit Solia <rohit.solia@gmail.com>
2024-02-22 11:00:46 -0600

    Updated verify step and create-release task

--- deploy/00_DPA.ts
+++ deploy/00_DPA.ts
@@ -1,4 +1,4 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { HardhatRuntimeEnvironment, HttpNetworkConfig } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const name = 'DescendingPriceAuction'
@@ -12,8 +12,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const deployed = await deploy(name, { from: deployer, log: true })
 
-  console.log('Verifying source code on the block explorer')
-  await run('verify', { address: deployed.address, noCompile: true })
+  const networkConfig = hre.network.config as unknown as HttpNetworkConfig
+  if (hre.network.name !== 'localhost' && !networkConfig.url.includes('localhost')) {
+    console.log('Verifying source code on the block explorer')
+    try {
+      await run('verify:verify', { address: deployed.address, noCompile: true })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      console.error('Contract verification failed for %s at %s', name, deployed.address, e.message)
+    }
+  }
 
   func.id = `${name}-${version}`
   return true
